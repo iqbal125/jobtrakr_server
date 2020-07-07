1c9abed737482569aeacc943a9b59a7f501b25ee
iqbal125 <iqbal125@yahoo.com>
2020-07-06 17:34:02 -0700

    create jobs table in sql schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,6 +1,21 @@
 
 CREATE TABLE users (
-  id PRIMARY SERIAL KEY,
+  id SERIAL PRIMARY KEY,
   username VARCHAR(255) UNIQUE,
   email VARCHAR(255),
+);
+
+
+CREATE TABLE jobs (
+  jid SERIAL PRIMARY KEY,
+  position VARCHAR(255),
+  company VARCHAR(255),
+  status VARCHAR(255),
+  date_applied VARCHAR(255),
+  point_of_contact VARCHAR(255),
+  poc_email VARCHAR(255),
+  poc_phone VARCHAR(255),
+  location VARCHAR(255),
+  notes VARCHAR,
+  user_id INT REFERENCES users(id)
 );
